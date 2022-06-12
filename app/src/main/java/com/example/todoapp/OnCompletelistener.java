6cdbceb03776d8dbc3964581a8bafa6658eb505a
baruna <baruna@NEBULA>
2022-06-12 16:00:06 +0700

    firebase

---
+++ app/src/main/java/com/example/todoapp/OnCompletelistener.java
@@ -0,0 +1,12 @@
+package com.example.todoapp;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Task;
+
+public class OnCompletelistener<T> implements com.google.android.gms.tasks.OnCompleteListener<Void> {
+    @Override
+    public void onComplete(@NonNull Task<Void> task) {
+
+    }
+}
